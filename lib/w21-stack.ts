ca9170f45fe1e981bdb2ab93fdb199e23c1b1fee
Jason Warren <contact.jwarren@gmail.com>
2024-10-25 16:10:37 +0100

    step 9 done

--- lib/w21-stack.ts
+++ lib/w21-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class W21Stack extends cdk.Stack {
+export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+		// Function Resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'W21Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+		// URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // CloudFormation Output
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    })
+
   }
 }
